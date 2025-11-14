204bf1278a722257cc01feb3e8df49079768377d
Luberri <fandresimahery@gmail.com>
2025-11-14 19:28:25 +0300

    ok

--- framework/src/main/java/com.itu.demo/test/Test2.java
+++
@@ -1,8 +0,0 @@
-package com.itu.demo.test;
-import com.itu.demo.annotations.HandleURL;
-public class Test2 {
-    @HandleURL("/string21")
-    public String sayHello() {
-        return "<h1>Hello from Test2!</h1>";
-    }
-}
